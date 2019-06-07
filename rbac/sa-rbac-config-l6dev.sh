64584c8c4a8448e16eb1f32392f0d756b3b5a91e
Elena Neroslavskaya <eneros@microsoft.com>
2019-06-07 05:16:54 +0000

    add dev role

--- rbac/sa-rbac-config-l6dev.sh
+++ rbac/sa-rbac-config-l6dev.sh
@@ -2,17 +2,23 @@
 
 SERVICE_ACCOUNT=$1
 NAMESPACE="$2"
+ROLE="$3"
+
+echo -e "\\nCreating role: ${ROLE} on namespace: ${NAMESPACE}"
+kubectl apply -f ${ROLE}-role.yaml
+
+echo -e "\\nCreating a role binding: ${ROLE} for ${SERVICE_ACCOUNT_NAME} on namespace: ${NAMESPACE}"
 
 cat <<EOF | kubectl apply -f -
 apiVersion: rbac.authorization.k8s.io/v1beta1
 kind: RoleBinding
 metadata:
-  name: ${SERVICE_ACCOUNT}-admin-binding
+  name: ${SERVICE_ACCOUNT}-dev-binding
   namespace: ${NAMESPACE}
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: ClusterRole
-  name: admin
+  name: ${ROLE}
 subjects:
   - kind: ServiceAccount
     name: ${SERVICE_ACCOUNT}
--- rbac/td-sa.sh
+++ rbac/td-sa.sh
@@ -3,16 +3,18 @@
 # Variables
 NAMESPACE=ernie
 SERVICEACCOUNT=ernie
+ROLE=l6dev
 
 # Set File Permissions
 chmod a+x sa-namespace-setup.sh
-chmod a+x sa-rbac-config.sh
+chmod a+x sa-rbac-config-admin.sh
+chmod a+x sa-rbac-config-l6dev.sh
 
 # Create Namespace
 kubectl create namespace $NAMESPACE
 # Setup Namespace & Configure Service Account for Namespace
 ./sa-namespace-setup.sh $SERVICEACCOUNT $NAMESPACE
 # Create AKS ServiceAccount, Role and Bindings
-./sa-rbac-config.sh $SERVICEACCOUNT $NAMESPACE
+./sa-rbac-config-$ROLE.sh $SERVICEACCOUNT $NAMESPACE $ROLE
 # Test Access by Getting List of ServiceAccounts and Pods in dev Namespace
 KUBECONFIG="kube/k8s-${SERVICEACCOUNT}-${NAMESPACE}-conf" kubectl get pods
